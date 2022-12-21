94f6f9e21f237891a4e5716829d4a522e823368c
Rafael Coelho <rafaellcoellho@gmail.com>
2022-12-21 18:36:41 -0300

    cria modelo de Ata e migration

--- atas/models.py
+++ atas/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Participante(models.Model):
     nome = models.CharField(max_length=100)
+
+
+class Ata(models.Model):
+    participantes = models.ManyToManyField(Participante)
